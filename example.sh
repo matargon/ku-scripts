7e80bd6407299a0a1c951ef383ad206d1b9e6024
Ernest Maiorov <erik.fr@yandex.ru>
2026-01-10 23:47:14 +0400

    .

--- example.sh
+++ example.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 USER=$(whoami)
 SRV=$1
-# ssh -1 /root/.ssh/id _ed25519 SUSER@$SRV 'mkdir /home/student/Desktop/try'
+# ssh -i /root/.ssh/id_ed25519 $USER@$SRV 'mkdir /home/student/Desktop/try'
 # rsync -av -e "ssh -i /root/.ssh/id_ed25519" /home/student/Desktop/try2 $USER@$SRV:/home/student/Desktop/
 # rsync -av /home/student/Desktop/try2 $USER@$SRV:/home/student/Desktop/
