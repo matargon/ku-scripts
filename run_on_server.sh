2a03b18d3f8ff484256348deac9111709746d07d
Ernest Maiorov <erik.fr@yandex.ru>
2026-01-10 22:26:28 +0400

    add gitignore

--- run_on_server.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "hello world"
-sleep 3
-# ssh -i ~/root/.ssh/id_ed25519 $USER@$srv 'mkdir /opt/backup/student' 
-# rsync -avz -e "ssh -i /root/.ssh/id_ed25519" /opt/backup/student/ $USER@$srv:/opt/backup/student
-# echo "something went wrong" >&2
-# exit 1
