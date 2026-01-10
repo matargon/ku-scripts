47876f701c27a0248d37db4d235dddacc390c4cf
Ernest Maiorov <erik.fr@yandex.ru>
2026-01-10 23:13:36 +0400

    .

--- example.sh
+++ example.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-
-echo "hello world"
-sleep 3
-# USER=$(whoami)
-# ssh -i ~/root/.ssh/id_ed25519 $USER@$srv 'mkdir /opt/backup/student' 
-# rsync -avz -e "ssh -i /root/.ssh/id_ed25519" /opt/backup/student/ $USER@$srv:/opt/backup/student
-# echo "something went wrong" >&2
+USER=S (whoani)
+ssh -1 /root/.ssh/id _ed25519 SUSER@$1 'mkdir /home/student/Desktop/try'
+#rsync -avz -e "ssh -i /root/.ssh/id_ed25519" /opt/backup/student/ $USER@$srv:/opt/backup/student
+# echo "something went wrong* >&2
 # exit 1
