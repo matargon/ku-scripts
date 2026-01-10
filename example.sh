4d98a93056b46f428aa115700090959245f77483
Ernest Maiorov <erik.fr@yandex.ru>
2026-01-10 22:35:49 +0400

    .

--- example.sh
+++ example.sh
@@ -2,6 +2,7 @@
 
 echo "hello world"
 sleep 3
+# USER=$(whoami)
 # ssh -i ~/root/.ssh/id_ed25519 $USER@$srv 'mkdir /opt/backup/student' 
 # rsync -avz -e "ssh -i /root/.ssh/id_ed25519" /opt/backup/student/ $USER@$srv:/opt/backup/student
 # echo "something went wrong" >&2
--- main.sh
+++ main.sh
@@ -95,7 +95,6 @@ print_summary() {
 #
 # Main
 #
-USER=$(whoami)
 successes=()
 failures=()
 local_ips=()
